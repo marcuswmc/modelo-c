bed2bbb0b5fca8fcaa35e26f248bac312fe04966
Yara Daudt <yaracrisalis@gmail.com>
2025-03-19 16:08:47 +0000

    feat: manifesto section

---
+++ src/components/manifesto-section.tsx
@@ -0,0 +1,54 @@
+export default function ManifestoSection() {
+  return (
+    <div className="w-full pt-20 pb-20 pl-24 pr-24 bg-custom-green">
+      <div>
+        <span className="pt-4.5 pb-4.5 pl-8 pr-8 rounded-full font-medium bg-custom-yellow text-title">
+          Manifesto
+        </span>
+      </div>
+
+      <div>
+        <h2 className="pt-20 pb-10 font-medium text-title text-white leading-tight">
+          A integração crítica <br />
+          precisa ganhar terreno
+        </h2>
+
+        <p className="text-white pr-30">
+          Precisamos avançar em práticas e reflexões integrativas que, de forma
+          crítica e responsável, favoreçam um enfrentamento mais efetivo das
+          desigualdades, injustiças sociais e dos desafios ambientais complexos.
+          Há tempos testemunhamos os efeitos negativos da predominância de uma
+          perspectiva de modernidade que fragmenta, isola e desconecta partes de
+          um todo - seja no nível individual, social ou ambiental. <br />
+          <br />
+          Vemos isso em ideias e produções voltadas para soluções temporárias ou
+          interesses particulares; em ações e decisões que ignoram as conexões
+          profundas entre elementos, seres e biomas em toda sua diversidade;
+          histórias contadas por apenas uma voz, desconexas de tantas outras que
+          compõem de modo imbricado um mesmo momento e realidade temporal; em
+          teorias e modelos isolados, rigidamente presos às suas próprias
+          certezas - e em tantos outros fenômenos que reforçam essa lógica
+          fragmentada. <br />
+          <br />
+          Diante desse cenário, temos duas escolhas: aceitar e reproduzir essas
+          estruturas ou buscar rupturas, novas perspectivas e mudanças que
+          promovam mais consciência, integração, justiça e equidade. Precisamos
+          de mais agentes sistêmicos de transformação, atuando com maturidade e
+          comprometidos com impactos positivos. Para isso, necessitamos de
+          modelos (e antimodelos) que ampliem nosso repertório de ação.
+          <br />
+          <br />
+          Este material reitera a nossa intenção de seguir contribuindo para o
+          campo de impacto socioambiental, para as organizações e para as
+          pessoas do ecossistema! Assim, as convidamos a explorar o Modelo C, a
+          refletir, construir e se comprometer com um olhar mais integrado e
+          sistêmico, que reconheça a complexidade e a interdependência entre
+          impacto e negócio, pessoas e territórios, presente e futuro. Que esse
+          seja um instrumento vivo, capaz de estimular novas perguntas,
+          fortalecer caminhos e impulsionar transformações que sejam, de fato,
+          relevantes para o mundo que queremos construir.
+        </p>
+      </div>
+    </div>
+  );
+}
