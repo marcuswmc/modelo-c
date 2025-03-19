533b5170b1d2a4bf0f1a5538c2daebb5b89c9b99
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-03-19 15:00:54 +0000

    update: page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero";
 
 export default function Home() {
-  return (    
-    <div className="w-full pl-24 pr-24 pt-20 flex-col gap-40">
-      <Hero/>
-    </div>
+  return (
+    <>
+      <div className="w-full pt-20 flex-col gap-40">
+        <Hero />
+      </div>
+    </>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,22 +4,22 @@ import heroBalance from "@/logos/hero-balance.png";
 import Image from "next/image";
 export default function Hero() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center pl-24 pr-24">
       <div className="flex flex-col gap-16 w-[50%]">
         <div>
           <Image src={logo} alt="Modelo C 2.0" />
         </div>
         <div className=" flex flex-col gap-5">
           <p className="text-sm">
-            O Modelo C é mais do que uma ferramenta — é um caminho para
+            <span className="font-medium">O Modelo C é mais do que uma ferramenta</span> — é um caminho para
             fortalecer negócios impacto socioambiental que transformam o mundo
             sem abrir mão da viabilidade econômica. Na versão 2.0, ele evolui
             sem perder sua essência: integrar de forma coerente impacto social e
             ambiental com lógica comercial e financeira.
           </p>
           <p>
-            A letra C continua a representar o que faz do Modelo C um
-            diferencial: Completo, Colaborativo, Conectado à Complexidade e
+            <span className="font-medium">A letra C continua a representar o que faz do Modelo C um
+            diferencial</span>: Completo, Colaborativo, Conectado à Complexidade e
             Compreensível. Um modelo para negócios que querem crescer com
             propósito e gerar mudanças relevantes.
           </p>
