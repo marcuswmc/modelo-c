376168afcc4befafb92029f1cd38c18b9a71e687
Yara Daudt <yaracrisalis@gmail.com>
2025-03-19 17:23:36 +0000

    feat: ponto de partida section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Hero from "@/components/hero";
 export default function Home() {
   return (
     <>
-      <div className="w-full pt-5 md:pt-20 lg:pt-20 flex-col ">
+      <div className="w-full pt-20 flex-col gap-40">
         <Hero />
       </div>
     </>
   );
-} 
+}
--- src/components/partida-section.tsx
+++ src/components/partida-section.tsx
@@ -1,3 +1,6 @@
+import icon from "../icons/bullet-icon.svg";
+import Image from "next/image";
+
 export default function PartidaFunction() {
   return (
     <div className="w-full pt-20 pb-20 pl-24 pr-24">
@@ -7,15 +10,44 @@ export default function PartidaFunction() {
         </span>
       </div>
 
-      <div>
-        <div>
+      <div className="flex justify-between items-start gap-x-16">
+        <div className="w-1/2">
           <h2 className="pt-20 pb-10 font-medium text-title">
             Para quê serve?
           </h2>
+          <div className="space-y-6">
+            {[
+              "Para apoiar o diagnóstico, a criação e gestão de negócios que buscam integrar efetivamente seu modelo de operação produtiva e comercial com a lógica de geração de impacto socioambiental positivo.",
+              "Para que negócios de impacto amadureçam, se tornando mais capazes de alcançar seus planos e ambições com fluxos financeiros e equipes saudáveis.",
+              "Para problematizar, provocar e inquietar empreendedores(as) e equipes sobre a forma como concebem e gerenciam os negócios em que estão inseridos.",
+              "Para alinhar times e parceiros de negócios de impacto ao redor de seu propósito e modelo de operação produtiva e comercial.",
+              "Para criar possibilidades de uma comunicação mais compreensível entre o negócio e seus diferentes públicos de interesse.",
+            ].map((text, index) => (
+              <div key={index} className="flex gap-x-5 items-start">
+                <Image src={icon} alt="Ícone de bullet"/>
+                <p>{text}</p>
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div>
+        <div className="w-1/2">
           <h2 className="pt-20 pb-10 font-medium text-title">Para quem é?</h2>
+          <div className="space-y-6">
+            {[
+              "Quem deseja criar ou lidera e opera um negócio de impacto - empreendedores/as e equipes.",
+              "Quem busca reinventar sua organização - organizações em transição para a lógica de negócio de impacto.",
+              "Quem assessora e apoia o amadurecimento de negócios de impacto - aceleradoras, incubadoras e outras intermediárias.",
+              "Quem financia negócios de impacto - investidores de impacto, fundos, entre outros.",
+              "Quem ensina a temática - docentes de instituições de ensino, mentores(as) e outras.",
+              "Qualquer pessoa interessada na temática de gestão de impacto.",
+            ].map((text, index) => (
+              <div key={index} className="flex gap-x-5 items-start">
+                <Image src={icon} alt="Ícone de bullet" width={20} height={20} />
+                <p>{text}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
