86412215ac04b480a1a1eace58cbd38e29613ada
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-03-20 19:25:52 +0000

    update: mobile nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,21 @@
 import logo from "@/logos/logo-modelo-c-header.svg"
 import Image from "next/image"
 import DesktopNav from "./desktop-nav"
+import MobileNav from "./mobile-nav"
 
 
 
 export default function Header() {
   return (
-    <div className="w-full h-20 pl-24 pr-24 pt-8 pb-8 justify-between items-center hidden md:flex lg:flex fixed top-0 left-0 bg-white z-50 shadow-md">
+    <div className="w-full h-20 pl-5 pr-5 md:pl-24 md:pr-24 pt-8 pb-8 justify-between items-center flex md:flex lg:flex fixed top-0 left-0 bg-white z-50 shadow-md">
       <div>
         <Image src={logo} alt="Modelo C 2.0"/>
       </div>
       <div className="hidden md:flex lg:flex">
         <DesktopNav />
+      </div>
+      <div className="md:hidden">
+        <MobileNav />
       </div>
     </div>
   )
--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,61 +1,88 @@
 "use client";
 
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 
-import logo from "@/logos/logo-modelo-c-header.svg";
 import Image from "next/image";
+import logo from "@/logos/logo-modelo-c-header.svg"
 
 const links = [
-  {
-    name: "Inicio",
-    path: "/",
-  },
-  {
-    name: "Manifesto",
-    path: "#manifesto",
-  },
-  {
-    name: "Modelo C 2.0",
-    path: "#modeloc",
-  },
-  {
-    name: "Realizadores",
-    path: "#realizadores",
-  },
+  { name: "Início", path: "#inicio" },
+  { name: "Manifesto", path: "#manifesto" },
+  { name: "Modelo C 2.0", path: "#modeloc" },
+  { name: "Realizadores", path: "#realizadores" },
 ];
 
 export default function MobileNav() {
-  const pathname = usePathname();
+  const [activeSection, setActiveSection] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleScroll = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    targetId: string
+  ) => {
+    event.preventDefault();
+    const section = document.querySelector(targetId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleScrollSpy = () => {
+      const scrollPosition = window.scrollY;
+
+      links.forEach(({ path }) => {
+        if (path.startsWith("#")) {
+          const section = document.querySelector(path);
+          if (section) {
+            const offsetTop =
+              section.getBoundingClientRect().top + window.scrollY;
+            const offsetBottom = offsetTop + section.clientHeight;
+
+            if (
+              scrollPosition >= offsetTop - 200 &&
+              scrollPosition < offsetBottom - 10
+            ) {
+              setActiveSection(path);
+            }
+          }
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScrollSpy);
+    return () => window.removeEventListener("scroll", handleScrollSpy);
+  }, []);
+
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
-        <Menu className="text-[32px] text-accent" />
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <button className="md:hidden p-2 border rounded-full border-custom-purple bg-custom-purple text-black">
+          <Menu size={22} />
+        </button>
       </SheetTrigger>
-      <SheetContent className="flex flex-col">
-        {/* logo */}
-        <div className="mt-32 mb-40 text-center text-2xl">
-          <Image src={logo} alt="Modelo C 2.0" />
-        </div>
-        {/* nav */}
-        <nav className="flex flex-col justify-center items-center gap-8">
-          {links.map((link, index) => {
-            return (
-              <Link
-                href={link.path}
-                key={index}
-                className={`${
-                  link.path === pathname &&
-                  "text-accent border-b-2 border-accent"
-                } text-xl capitalize hover:text-accent transition-all`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
+      <SheetContent side="left" className="bg-white p-5 flex flex-col justify-between pb-20">
+        <nav className="flex flex-col gap-4 pt-20">
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              onClick={(event) => handleScroll(event, link.path)}
+              className={`text-md font-medium ${
+                activeSection === link.path
+                  ? "bg-custom-purple border-custom-purple text-white"
+                  : "border-black text-black"
+              } pt-1.5 pb-1.5 pl-5 pr-5 rounded-full border font-medium text-sm cursor-pointer transition-colors`}
+            >
+              {link.name}
+            </Link>
+          ))}
         </nav>
+        <Image src={logo} alt="Modelo C 2.0" width={300}/>
       </SheetContent>
     </Sheet>
   );
