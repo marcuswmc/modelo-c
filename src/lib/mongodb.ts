2eaf4cb6f9d88da307666cd64e5e1b8c0488bee4
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-03-21 15:07:31 +0000

    update: mongodb cached

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,21 +3,34 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error("Defina a variável de ambiente MONGODB_URI");
+  throw new Error("MONGODB_URI not exist");
 }
 
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
 export const connectToDatabase = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return;
+  if (cached.conn) {
+    console.log("using existent MongoDB");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    console.log("new conection to MONGODB");
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "landing_page_db",
+      bufferCommands: false,
+    });
   }
 
   try {
-    await mongoose.connect(MONGODB_URI, {
-      dbName: "landing_page_db",
-    });
-    console.log("✅ Conectado ao MongoDB");
+    cached.conn = await cached.promise;
+    console.log("MongoDB conected");
   } catch (error) {
-    console.error("❌ Erro ao conectar ao MongoDB:", error);
-    process.exit(1);
+    console.error("Erro to conect MongoDB:", error);
+    cached.promise = null;
+    throw error;
   }
+
+  (global as any).mongoose = cached; 
+  return cached.conn;
 };
