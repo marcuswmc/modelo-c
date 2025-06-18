249855a0cfe6083c3e823e2652f48b9e4b08dabb
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-06-18 15:35:09 +0100

    update: redirect en cmodel v3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,26 @@
 import { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
 
-const intlMiddleware = createMiddleware(routing);
+const intlMiddlewareForCModel = createMiddleware({
+  locales: ['en', 'pt', 'es'],
+  defaultLocale: 'en',
+  localePrefix: 'as-needed'
+});
 
-const supportedLocales = ['pt', 'en', 'es'];
+const intlMiddlewareDefault = createMiddleware({
+  locales: ['pt', 'en', 'es'],
+  defaultLocale: 'pt',
+  localePrefix: 'as-needed'
+});
 
 export default function middleware(request: NextRequest) {
   const hostname = request.headers.get('host');
-  const pathname = request.nextUrl.pathname;
 
-  const localePrefix = pathname.split('/')[1];
-  const hasLocale = supportedLocales.includes(localePrefix);
-
-  // Caso o domínio seja cmodel.co e a URL não tenha prefixo de idioma,
-  // forçamos o redirecionamento para EN
-  if (hostname && hostname.includes('cmodel.co') && !hasLocale) {
-    const url = request.nextUrl.clone();
-    url.pathname = `/en${pathname}`;
-    return Response.redirect(url, 307);
+  if (hostname?.includes('cmodel.co')) {
+    return intlMiddlewareForCModel(request);
   }
 
-  return intlMiddleware(request);
+  return intlMiddlewareDefault(request);
 }
 
 export const config = {
