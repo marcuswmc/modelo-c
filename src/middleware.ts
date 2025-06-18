caf85f9497eb458016ae9bb9ba3e4a6537e9fcb5
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-06-18 15:10:35 +0100

    update: redirect en cmodel

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,22 @@ import { routing } from './i18n/routing';
 
 const intlMiddleware = createMiddleware(routing);
 
+// Lista de idiomas suportados
+const supportedLocales = ['en','es', 'pt'];
+
 export default function middleware(request: NextRequest) {
   const hostname = request.headers.get('host');
 
   if (hostname && hostname.includes('cmodel.co')) {
-    const url = request.nextUrl.clone();
+    const pathname = request.nextUrl.pathname;
 
-    if (!url.pathname.startsWith('/en')) {
-      url.pathname = `/en${url.pathname}`;
+    const localePrefix = pathname.split('/')[1];
+    const hasLocale = supportedLocales.includes(localePrefix);
+
+    // Redireciona para inglês somente se não houver prefixo de idioma
+    if (!hasLocale) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/en${pathname}`;
       return Response.redirect(url, 307);
     }
   }
